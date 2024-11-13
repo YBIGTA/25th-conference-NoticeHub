86d1d70eb0455295f1c331439d889a07c79a50f5
allofjw <jw990416@yonsei.ac.kr>
2024-11-13 20:51:01 +0900

    feat : create spring projecet
    create spring project

---
+++ src/main/java/com/example/devmaster/DevMasterApplication.java
@@ -0,0 +1,12 @@
+package com.example.devmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMasterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevMasterApplication.class, args);
+    }
+
+}
