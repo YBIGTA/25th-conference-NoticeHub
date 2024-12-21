d834f72b86eb1a6b4fd6730f5456a3c452b5239a
allofjw <jw990416@yonsei.ac.kr>
2024-12-21 15:51:11 +0900

    refactor : delete dummy code

--- dev-master/README.md
+++
@@ -1,2 +0,0 @@
-# 25th-conference-NoticeHub
-24-2학기 컨퍼런스 - NoticeHub
--- dev-master/src/main/java/com/example/devmaster/controller/MainRestController.java
+++
@@ -1,6 +0,0 @@
-package com.example.devmaster.controller;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MainRestController {
-}
